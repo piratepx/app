62f75d050be54cf24d03e18182d5a10222f6a281
Justin Stayton <hello@piratepx.com>
2020-10-02 21:37:38 -0400

    Enable CORS on /ship route

--- api/routes/ship.js
+++ api/routes/ship.js
@@ -5,56 +5,63 @@ const Project = require('@/models/project')
 const px = require('@/lib/px')
 
 module.exports = async (fastify) => {
-  fastify.get(
-    '/ship',
-    {
-      attachValidation: true,
-      errorHandler(error, request) {
-        request.log.warn(error)
-      },
-      schema: {
-        querystring: {
-          type: 'object',
-          properties: {
-            p: Project.jsonSchema.properties.id,
-            i: Count.jsonSchema.properties.identifier,
+  fastify.register(
+    async (fastify) => {
+      fastify.register(require('fastify-cors'))
+
+      fastify.get(
+        '/',
+        {
+          attachValidation: true,
+          errorHandler(error, request) {
+            request.log.warn(error)
           },
-          required: ['p'],
-        },
-        response: {
-          200: {
-            type: 'string',
-            contentEncoding: 'binary',
-            contentMediaType: 'image/gif',
+          schema: {
+            querystring: {
+              type: 'object',
+              properties: {
+                p: Project.jsonSchema.properties.id,
+                i: Count.jsonSchema.properties.identifier,
+              },
+              required: ['p'],
+            },
+            response: {
+              200: {
+                type: 'string',
+                contentEncoding: 'binary',
+                contentMediaType: 'image/gif',
+              },
+            },
           },
         },
-      },
+        async (request, reply) => {
+          reply
+            .type('image/gif')
+            .headers({
+              'Cache-Control': 'private, max-age=0, no-cache, no-store',
+              Expires: '-1',
+              Pragma: 'no-cache',
+            })
+            .send(px)
+
+          if (request.validationError) {
+            throw request.validationError
+          }
+
+          const data = new CountRequestData({ request })
+
+          const project = await Project.query().findById(data.projectID)
+
+          if (!project) {
+            throw new Error('Project not found')
+          }
+
+          const incrementor = new CountIncrementor({ data, project })
+
+          await incrementor.increment()
+        }
+      )
     },
-    async (request, reply) => {
-      reply
-        .type('image/gif')
-        .headers({
-          'Cache-Control': 'private, max-age=0, no-cache, no-store',
-          Expires: '-1',
-          Pragma: 'no-cache',
-        })
-        .send(px)
-
-      if (request.validationError) {
-        throw request.validationError
-      }
-
-      const data = new CountRequestData({ request })
-
-      const project = await Project.query().findById(data.projectID)
-
-      if (!project) {
-        throw new Error('Project not found')
-      }
-
-      const incrementor = new CountIncrementor({ data, project })
-
-      await incrementor.increment()
-    }
+    { prefix: '/ship' }
   )
 }
