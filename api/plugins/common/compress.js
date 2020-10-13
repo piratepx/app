c3762fd90092c9559ec835d117ddf19ad88db110
Justin Stayton <hello@piratepx.com>
2020-10-13 09:31:20 -0400

    Disable Brotli compression for the time being

--- api/plugins/common/compress.js
+++ api/plugins/common/compress.js
@@ -3,12 +3,7 @@ const zlib = require('zlib')
 module.exports = require('fastify-compress')
 
 module.exports.autoConfig = {
-  brotliOptions: {
-    params: {
-      [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
-      [zlib.constants.BROTLI_PARAM_QUALITY]: 1,
-    },
-  },
+  encodings: ['gzip', 'deflate', 'identity'],
   zlibOptions: {
     level: zlib.constants.Z_BEST_SPEED,
   },
